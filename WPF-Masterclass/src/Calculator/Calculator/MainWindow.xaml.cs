138126873b60cd3a932b4d21b67973cc1db60a46
Gabriel Guerra <gabriel@inlive.com.br>
2025-12-10 17:48:30 -0300

    Handler

--- WPF-Masterclass/src/Calculator/Calculator/MainWindow.xaml.cs
+++ WPF-Masterclass/src/Calculator/Calculator/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+
+            resultLabel.Content = "0";
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button numberButton = (Button) sender;
+
+            string numero = numberButton.Content.ToString();
+
+            if(resultLabel.Content.ToString() == "0") 
+            {
+
+                resultLabel.Content = $"{numero}";
+            }
+            else 
+            {
+                resultLabel.Content += $"{numero}";
+            }
+        }
+
+        private void OpButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eqButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
